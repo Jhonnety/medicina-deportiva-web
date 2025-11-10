b826a25d4d9bbb7fad442accd91c397e707b84c6
Jhonnety <jhonestebanvelasquezgomez@gmail.com>
2025-11-09 21:08:02 -0500

    fix bug on glosary

--- lib/i18n/dictionaries.ts
+++ lib/i18n/dictionaries.ts
@@ -3,8 +3,12 @@ import type { Locale } from './config';
 const dictionaries = {
   es: () => import('./dictionaries/es.json').then((module) => module.default),
   en: () => import('./dictionaries/en.json').then((module) => module.default),
-};
+} as const;
 
-export const getDictionary = async (locale: Locale) => dictionaries[locale]();
+export const getDictionary = async (locale: Locale | string) => {
+  const key = (locale === 'en' ? 'en' : 'es') as keyof typeof dictionaries;
+  const loader = dictionaries[key];
+  return loader();
+};
 
 
