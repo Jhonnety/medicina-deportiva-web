bbdd815aac1629672e4a70a23c1470dde7c14c97
Jhonnety <jhonestebanvelasquezgomez@gmail.com>
2026-03-01 00:10:22 -0500

    fix build

--- app/[lang]/atletas/page.tsx
+++ app/[lang]/atletas/page.tsx
@@ -1,23 +1,17 @@
-import HeroAthleteSection from '@/components/athletes/HeroAthleteSection';
-import AthleteAudienceSection from '@/components/athletes/AthleteAudienceSection';
-import AthleteProcessSection from '@/components/athletes/AthleteProcessSection';
-import AthleteTreatmentsSection from '@/components/athletes/AthleteTreatmentsSection';
-import AthleteDifferentialsSection from '@/components/athletes/AthleteDifferentialsSection';
-import AthleteTestimonialsSection from '@/components/athletes/AthleteTestimonialsSection';
-import AthleteFAQSection from '@/components/athletes/AthleteFAQSection';
-import AthleteFinalCTASection from '@/components/athletes/AthleteFinalCTASection';
-
 export default function AthletesPage() {
   return (
-    <>
-      <HeroAthleteSection />
-      <AthleteAudienceSection />
-      <AthleteProcessSection />
-      <AthleteTreatmentsSection />
-      <AthleteDifferentialsSection />
-      <AthleteTestimonialsSection />
-      <AthleteFAQSection />
-      <AthleteFinalCTASection />
-    </>
+    <div>
+      {/* 
+        <HeroAthleteSection />
+        <AthleteAudienceSection />
+        <AthleteProcessSection />
+        <AthleteTreatmentsSection />
+        <AthleteDifferentialsSection />
+        <AthleteTestimonialsSection />
+        <AthleteFAQSection />
+        <AthleteFinalCTASection />
+      */}
+      <h1>Página de Atletas en Construcción</h1>
+    </div>
   );
 }
